86e89a325d3238cd120e9368149353385a075c0c
monge1h <jlo4321jl@gmail.com>
2020-08-05 21:41:41 -0600

    Create test directory and test_base.py

---
+++ tests/test_base.py
@@ -0,0 +1,19 @@
+from flask_testing import TestCase
+from flask import current_app, url_for
+
+from main import app
+
+
+class MainTest(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+
+        return app
+
+    def test_app_exist(self):
+        self.assertIsNotNone(current_app)
+
+    def test_index_get(self):
+        self.client.get(url_for('index'))
+
+        self.assertTemplateUsed('home.html')
