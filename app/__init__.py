a6eacf5531460944993c7468935b5e02cd2fa6ab
monge1h <jlo4321jl@gmail.com>
2020-08-09 18:12:41 -0600

    Add csrf and app.config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+from .config import Config
 
 
 def create_app():
     app = Flask(__name__)
+    CSRFProtect(app)
+    app.config.from_object(Config)
 
     return app
