97284b598a9ac1b9614f9d7d413fd8311a27b289
monge1h <jlo4321jl@gmail.com>
2020-08-14 19:43:16 -0600

    Add POST method support to indes function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import create_app
-from flask import render_template
+from flask import render_template, url_for, redirect
 from app.forms import PasteBinForm
 import unittest
 
@@ -13,13 +13,18 @@ def test():
     unittest.TextTestRunner().run(tests)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     paste_bin_form = PasteBinForm()
 
     context = {
         'paste_bin_form': paste_bin_form
     }
+
+    if paste_bin_form.validate_on_submit():
+        paste_bin = paste_bin_form.paste.data
+        return redirect(url_for('paste_bin', paste_id=paste_bin))
+
     return render_template('home.html', **context)
 
 
